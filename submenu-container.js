296dfe1566c83b1f290acf949ba8af7f025fbd50
seufster <seufster@gmail.com>
2018-05-06 14:51:44 -0400

    update

---
+++ submenu-container.js
@@ -0,0 +1,68 @@
+/*
+* Author Edward Seufert
+*/
+'use-strict';
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import {withRouter} from "react-router";
+import * as appPrefActions from '../../core/common/apppref-actions';
+import fuLogger from '../../core/common/fu-logger';
+import SubMenuView from '../../memberView/submenu/submenu-view';
+
+
+class SubMenuContainer extends Component {
+	constructor(props) {
+		super(props);
+		this.changeTab = this.changeTab.bind(this);
+	}
+
+	componentDidMount() {
+		//this.props.actions.initMember();
+	}
+
+	changeTab(index) {
+      this.props.history.replace(index);
+  }
+
+  render() {
+			fuLogger.log({level:'TRACE',loc:'SubMenuContainer::render',msg:"Hi there"});
+			const path = this.props.history.location.pathname;
+			let topMenus = this.props.appMenus[this.props.appPrefs.memberMenu];
+			let children = [];
+			if (topMenus != null) {
+				for (let m = 0; m < topMenus.length; m++) {
+					if (topMenus[m].values[0].rendered) {
+						if (path.includes(topMenus[m].values[0].href)){
+							if (topMenus[m].children != null && topMenus[m].children.length > 0){
+								children = topMenus[m].children;
+							}
+						}
+					}
+				}
+			}
+      return (
+				<SubMenuView menus={children} changeTab={this.changeTab}/>
+			);
+  }
+}
+
+SubMenuContainer.propTypes = {
+	appMenus: PropTypes.object,
+	appPrefs: PropTypes.object,
+	lang: PropTypes.string,
+	appGlobal: PropTypes.object,
+	actions: PropTypes.object,
+	history: PropTypes.object
+};
+
+function mapStateToProps(state, ownProps) {
+  return {appMenus:state.appMenus, lang:state.lang, appPrefs:state.appPrefs};
+}
+
+function mapDispatchToProps(dispatch) {
+  return { actions:bindActionCreators(appPrefActions,dispatch) };
+}
+
+export default withRouter(connect(mapStateToProps,mapDispatchToProps)(SubMenuContainer));
