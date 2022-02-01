1ab528b8cf0026b61edab3a923b428e363428007
Edward Seufert <seufster@gmail.com>
2022-01-31 21:33:39 -0500

    Convert to hooks

--- submenu-container.js
+++ submenu-container.js
@@ -2,66 +2,44 @@
 * Author Edward Seufert
 */
 'use-strict';
-import React, {Component} from 'react';
-import PropTypes from 'prop-types';
-import {connect} from 'react-redux';
-import {bindActionCreators} from 'redux';
-import {withRouter} from "react-router";
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { Routes, Route, useNavigate, useLocation } from "react-router-dom";
 import * as appPrefActions from '../../core/common/apppref-actions';
 import fuLogger from '../../core/common/fu-logger';
 import SubMenuView from '../../memberView/submenu/submenu-view';
 
 
-class SubMenuContainer extends Component {
-	constructor(props) {
-		super(props);
-		this.changeTab = this.changeTab.bind(this);
-	}
+function SubMenuContainer() {
+	const session = useSelector((state) => state.session);
+	const appMenus = useSelector((state) => state.appMenus);
+	const appPrefs = useSelector((state) => state.appPrefs);
+	const dispatch = useDispatch();
+	const location = useLocation();
+	const navigate = useNavigate();
+	
+	const changeTab = (code,index) => {
+      navigate(index);
+  	}
 
-	componentDidMount() {
-		//this.props.actions.initMember();
-	}
-
-	changeTab(index) {
-      this.props.history.replace(index);
-  }
-
-  render() {
-			fuLogger.log({level:'TRACE',loc:'SubMenuContainer::render',msg:"Hi there"});
-			const path = this.props.history.location.pathname;
-			let topMenus = this.props.appMenus[this.props.appPrefs.memberMenu];
-			let children = [];
-			if (topMenus != null) {
-				for (let m = 0; m < topMenus.length; m++) {
-					if (topMenus[m].values[0].rendered) {
-						if (path.includes(topMenus[m].values[0].href)){
-							if (topMenus[m].children != null && topMenus[m].children.length > 0){
-								children = topMenus[m].children;
-							}
-						}
+	fuLogger.log({level:'TRACE',loc:'SubMenuContainer::render',msg:"Hi there"});
+	const path = location.pathname;
+	let topMenus = appMenus[appPrefs.memberMenu];
+	let children = [];
+	if (topMenus != null) {
+		for (let m = 0; m < topMenus.length; m++) {
+			if (topMenus[m].values[0].rendered) {
+				if (path.includes(topMenus[m].values[0].href)){
+					if (topMenus[m].children != null && topMenus[m].children.length > 0){
+						children = topMenus[m].children;
 					}
 				}
 			}
-      return (
-				<SubMenuView menus={children} changeTab={this.changeTab}/>
-			);
-  }
+		}
+	}
+    return (
+		<SubMenuView menus={children} changeTab={this.changeTab}/>
+	);
 }
 
-SubMenuContainer.propTypes = {
-	appMenus: PropTypes.object,
-	appPrefs: PropTypes.object,
-	lang: PropTypes.string,
-	actions: PropTypes.object,
-	history: PropTypes.object
-};
-
-function mapStateToProps(state, ownProps) {
-  return {appMenus:state.appMenus, lang:state.lang, appPrefs:state.appPrefs};
-}
-
-function mapDispatchToProps(dispatch) {
-  return { actions:bindActionCreators(appPrefActions,dispatch) };
-}
-
-export default withRouter(connect(mapStateToProps,mapDispatchToProps)(SubMenuContainer));
+export default SubMenuContainer;
